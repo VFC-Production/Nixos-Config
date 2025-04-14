04e8cf7af0ebb4701d0d72a3625165537d816433
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-04-14 10:42:30 -0400

    update micboard docker config

--- service-config/docker/micboard.nix
+++ service-config/docker/micboard.nix
@@ -4,10 +4,11 @@
   config.virtualisation.oci-containers.containers = {
     micboard = {
       image = "ghcr.io/karlcswanson/micboard:0.8.7-updates"; # Container Image.
-      ports = ["0.0.0.0:8058:8058"]; # Expose 8058 on all interfaces, 0.0.0.0, and _not_ 127.0.0.1
+      #ports = ["0.0.0.0:8058:8058"]; # Expose 8058 on all interfaces, 0.0.0.0, and _not_ 127.0.0.1
       volumes = [ # Mount config so state persists reboots
         "/system-data/serviceState/:/root/.local/share/micboard"
       ];
+      estraOptions = "--network=host"; #Just let the container access host ports. Probably need this to pick up telemetry anyways.
     };
   };
 }
