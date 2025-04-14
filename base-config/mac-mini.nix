f0dcb0a840579967e4c18b3fdc4f339b1f3b8693
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-04-14 16:18:19 -0400

    Update mac config

--- base-config/mac-mini.nix
+++ base-config/mac-mini.nix
@@ -1,39 +1,52 @@
+
 { outputs, inputs, lib, config, pkgs, modulesPath, ... }:{
   imports = [ 
     (modulesPath + "/installer/scan/not-detected.nix") # Pick up kernel modules/drivers that aren't already under boot.initrd
     ];
+  specialisation = {
+    runtime = {
+      inheritParentConfig = false;
+      configuration = {
+        networking.networkmanager.enable = true; # NMTUI is dumb easy to use.
 
-  networking.networkmanager.enable = true; # NMTUI is dumb easy to use.
-
-  time.timeZone = "America/New_York"; # Set Timezone
+          time.timeZone = "America/New_York"; # Set Timezone
 
 
-  nix = {
-# I've been deploying with flakes
-    extraOptions = ''
-      experimental-features = nix-command flakes 
-    '';
-  };
+          nix = {
+        # I've been deploying with flakes
+            extraOptions = ''
+              experimental-features = nix-command flakes 
+            '';
+          };
 
-# Define a user
-  users.users = {
+        # Define a user
+          users.users = {
 
-    user = {
-      isNormalUser = true;
-      home = "/home/user";
-      description  = "general system user";
-      uid = 1000; 
-      extraGroups = [ "wheel" "docker" "networkmanager" "storage" ]; 
-      hashedPassword = "$y$j9T$gfos6aXIGxx6T9SZXIGft/$CuCPpN0BGI.YGe3qsrnZyMSXgDyP6uIVPpACXsXZyY1"; #  mkpasswd
+            user = {
+              isNormalUser = true;
+              home = "/home/user";
+              description  = "general system user";
+              uid = 1000; 
+              extraGroups = [ "wheel" "docker" "networkmanager" "storage" ]; 
+            };
+          };
+
+          system.stateVersion = "24.11";
+          boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+          boot.initrd.kernelModules = [ ];
+          boot.kernelModules = [ "kvm-intel" ];
+          boot.extraModulePackages = [ ];
+
+          nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+          hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+      };
     };
-  };
-
-  system.stateVersion = "24.11";
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    Maintenance Mode = {
+      configuration = {
+        services.tailscale.enable = true; #
+        users.users.user.hashedPassword = "$y$j9T$gfos6aXIGxx6T9SZXIGft/$CuCPpN0BGI.YGe3qsrnZyMSXgDyP6uIVPpACXsXZyY1"; #  mkpasswd
+        systemd.services."cage@".enable = lib.mkForce "false"; # Force Disable Cage UI.
+      };
+    };
+  }; 
 }
