8f55f0fd825dba3b2b62420047a0dd658d584278
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-04-16 14:56:09 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
             ./base-config/mac-mini.nix # Base system config. Meant to be extended with below lines.
             ./service-config/docker/containerd.nix # Configure docker daemon.
             ./service-config/docker/micboard.nix # Run Micboard with docker via systemd.
-            ./service-config/UI/micboard-kiosk.nix # Run a basic Cage session with epiphany. Allows micboard to just be the mac and a display.
+            ./service-config/UI/micboard-openbox-kiosk.nix # Run a basic Cage session with epiphany. Allows micboard to just be the mac and a display.
         ];
       };
       bootstrap = nixpkgs.lib.nixosSystem {
